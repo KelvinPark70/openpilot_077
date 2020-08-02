8b10119586adaf21feba603af4e774b2f1631801
chobyounghak <61299886+chobyounghak@users.noreply.github.com>
2020-08-02 09:46:21 +0900

    dd

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -171,8 +171,8 @@ class CarController():
       can_sends.append(create_mdps12(self.packer, frame, CS.mdps12))                                   
 
 
-    str_log1 = 'torg:{:3.0f}/{:3.0f}'.format( apply_steer, new_steer )
-    str_log2 = 'max={:.0f} tm={:.1f} '.format( apply_steer_limit, self.timer1.sampleTime()  )
+    str_log1 = 'torg:{:>3.0f}/{:>3.0f}'.format( apply_steer, new_steer )
+    str_log2 = 'max={:>3.0f} tm={:>5.1f} '.format( apply_steer_limit, self.timer1.sampleTime()  )
     trace1.printf( '{} {}'.format( str_log1, str_log2 ) )
 
     lfa_usm = CS.lfahda["LFA_USM"]
