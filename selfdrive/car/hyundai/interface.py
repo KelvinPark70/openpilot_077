62bab31c19d3a9fc68ed01e45009d3d8cd00b7df
chobyounghak <61299886+chobyounghak@users.noreply.github.com>
2020-08-16 16:10:39 +0900

    dd

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -38,8 +38,8 @@ class CarInterface(CarInterfaceBase):
       ret.mass = 1675. + STD_CARGO_KG
       ret.wheelbase = 2.845
       ret.steerRatio = 14.0   #12.5
-      ret.steerMaxBP = [20*CV.KPH_TO_MS, 30*CV.KPH_TO_MS, 70*CV.KPH_TO_MS]
-      ret.steerMaxV = [1.2, 1.1, 1.0]
+      ret.steerMaxBP = [30*CV.KPH_TO_MS, 70*CV.KPH_TO_MS]
+      ret.steerMaxV = [1.2, 1.0]
       ret.lateralTuning.pid.kf = 0.000005
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.05]]
