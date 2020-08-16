68449a9960555e07ebe5375d5d545b792fe2815d
chobyounghak <61299886+chobyounghak@users.noreply.github.com>
2020-08-16 15:53:19 +0900

    dd

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -391,7 +391,7 @@ void handle_message(UIState *s, SubMaster &sm) {
        scene.mpc_x[i] = x_list[i];
        scene.mpc_y[i] = y_list[i];
      }
-     s->livempc_or_radarstate_changed = true;
+     //s->livempc_or_radarstate_changed = true;
   }
   if (sm.updated("uiLayoutState")) {
     auto data = sm["uiLayoutState"].getUiLayoutState();
